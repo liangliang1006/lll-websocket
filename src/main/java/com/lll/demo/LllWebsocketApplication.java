7ee742d37dd323c2ea08f0552fbba37775607296
liangliang <liull@xiaoqiedu.cn>
2019-12-11 18:58:18 +0800

    初始化demo

---
+++ src/main/java/com/lll/demo/LllWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.lll.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LllWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LllWebsocketApplication.class, args);
+	}
+
+}
